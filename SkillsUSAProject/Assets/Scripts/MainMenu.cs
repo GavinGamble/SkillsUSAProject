e3e5f0be49c602b6693fe3be42f5718b0758bc48
GavinGamble <710145@student.dusd.net>
2024-01-15 20:05:44 -0800

    1/15/2024
    Title Screen finished. Added settings menu (imcomplete) Quit button, start button fixed scene created castles created sandbox added sounds to scene.

---
+++ SkillsUSAProject/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void OpenSettings()
+    {
+
+    }
+    public void OpenSandbox()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+}
