f624d9dc44801dabc9879a91ae311596986e5c22
Louie1033 <louissantibanez22@gmail.com>
2024-01-18 15:19:33 -0800

    Fixed tower placement
    -cant place towers inside of each other
    -checks to see if placing is possible
    -just need to fix a couple more colliders and fix positioning

--- SkillsUSAProject/Assets/Scripts/PlaceTowers.cs
+++ SkillsUSAProject/Assets/Scripts/PlaceTowers.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlaceTowers : MonoBehaviour
 {
+    [SerializeField] private LayerMask CheckPlacement;
+    [SerializeField] private LayerMask PlacementCollider;
     [SerializeField] private Camera PlayersCamera;
     private GameObject TowerToPlace;
     // Start is called before the first frame update
@@ -18,14 +20,28 @@ public class PlaceTowers : MonoBehaviour
         if(TowerToPlace != null)
         {
             Ray camray = PlayersCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit HitInfo;
 
-            if(Physics.Raycast(camray, out RaycastHit hitInfo, 100f))
+            if(Physics.Raycast(camray, out  HitInfo, 100f, PlacementCollider))
             {
-                TowerToPlace.transform.position = hitInfo.point;
+                TowerToPlace.transform.position = HitInfo.point;
             }
-            if(Input.GetMouseButtonDown(0))
+            if(Input.GetMouseButtonDown(0) && HitInfo.collider.gameObject != null)
             {
-                TowerToPlace = null;
+                if(!HitInfo.collider.gameObject.CompareTag("CantPlace"))
+                {
+                    BoxCollider TowersCollider = TowerToPlace.gameObject.GetComponent<BoxCollider>();
+                    TowersCollider.isTrigger = true;
+                    Vector3 BoxCenter = TowerToPlace.gameObject.transform.position + TowersCollider.center;
+                    Vector3 HalfExtent = TowersCollider.size / 2;
+                    if (!Physics.CheckBox(BoxCenter, HalfExtent, Quaternion.identity, CheckPlacement, QueryTriggerInteraction.Ignore))
+                    {
+                        TowersCollider.isTrigger = false;
+                        TowerToPlace = null;
+                    }
+                }
+                
+                
             }
         }
     }
