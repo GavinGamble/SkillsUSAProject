66cdcbd64f4b4d53a52411a0d13fcca51c6a6cd7
GavinGamble <710145@student.dusd.net>
2024-03-21 13:11:09 -0700

    3/21/24

--- SkillsUSAProject/Assets/Scripts/Timer.cs
+++ SkillsUSAProject/Assets/Scripts/Timer.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TextMeshProUGUI timerText;
+    public float updateInterval = 1f;
+
+    private float elapsedTime = 0f;
+
     void Start()
     {
-        
+        // Start the timer
+        InvokeRepeating("UpdateTimer", 0f, updateInterval);
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateTimer()
     {
-        
+        // Update elapsed time
+        elapsedTime += updateInterval;
+
+        // Format and display the timer text
+        timerText.text = FormatTime(elapsedTime);
+    }
+
+    string FormatTime(float timeInSeconds)
+    {
+        int hours = Mathf.FloorToInt(timeInSeconds / 3600);
+        int minutes = Mathf.FloorToInt((timeInSeconds % 3600) / 60);
+        int seconds = Mathf.FloorToInt(timeInSeconds % 60);
+
+        if (hours > 0)
+        {
+            return string.Format("{0:00}:{1:00}:{2:00}", hours, minutes, seconds);
+        }
+        else if (minutes > 0)
+        {
+            return string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+        else
+        {
+            return string.Format("{0}", seconds);
+        }
     }
 }
+
+
