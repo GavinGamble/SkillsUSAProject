ea376f6acd39b71a01f066f3193075050ef77a2b
GavinGamble <710145@student.dusd.net>
2024-03-31 21:19:00 -0700

    Made Level One Menu
    3/31/24

---
+++ SkillsUSAProject/Assets/MenuStuff/LevelOne/LevelOnePauseMenu.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class LevelOnePauseMenu : MonoBehaviour
+{
+    public GameObject PauseMenu;
+    public AudioMixer LevelOneMixer;
+    public bool isPaused;
+    public void SetVolume(float volume)
+    {
+        LevelOneMixer.SetFloat("Volume", volume);
+    }
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    //Pause and Resume
+    public void Start()
+    {
+       PauseMenu.SetActive(false);
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+;
+        }
+    }
+
+    public void PauseGame()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+        isPaused = false;
+    }
+}
