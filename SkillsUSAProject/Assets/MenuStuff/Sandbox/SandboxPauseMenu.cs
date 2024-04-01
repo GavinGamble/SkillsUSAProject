d18c6f8a3efe22ecffa21131291ad201a3ae9edf
GavinGamble <710145@student.dusd.net>
2024-03-31 23:44:07 -0700

    Added Music To Each Level Fixed all main menus
    Tomorrow I will add Resolution, graphics settings, Music skip/ go back. I will make new sprites for the Icons in menus.
    Good night

---
+++ SkillsUSAProject/Assets/MenuStuff/Sandbox/SandboxPauseMenu.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class SandboxPauseMenu : MonoBehaviour
+{
+    public GameObject SBPauseMenu;
+    public AudioMixer SandBoxMixer;
+    public static bool isPaused;
+    public void SetVolume(float volume)
+    {
+        SandBoxMixer.SetFloat("LevelTwoVolume", volume);
+    }
+    public void QuitGame()
+    {
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit();
+    #endif
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    //Pause and Resume
+    public void Start()
+    {
+        SBPauseMenu.SetActive(false);
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+;
+        }
+    }
+
+    public void PauseGame()
+    {
+        SBPauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        SBPauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+        isPaused = false;
+    }
+}
